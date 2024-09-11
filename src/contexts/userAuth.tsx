36cdea18549574c5214104220a2d13ea03bd2a1f
Adryel Santos <118002541+AdryelJr@users.noreply.github.com>
2024-09-11 10:44:11 -0300

    up code profile complete

--- src/contexts/userAuth.tsx
+++ src/contexts/userAuth.tsx
@@ -1,7 +1,13 @@
-import { signInWithPopup, GoogleAuthProvider, updateProfile, createUserWithEmailAndPassword, signInWithEmailAndPassword as firebaseSignInWithEmailAndPassword } from "firebase/auth";
+import {
+    signInWithPopup,
+    GoogleAuthProvider,
+    updateProfile,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword as firebaseSignInWithEmailAndPassword,
+    signOut
+} from "firebase/auth";
 import { getDatabase, ref, set, get } from "firebase/database";
 import { ReactNode, createContext, useEffect, useState } from 'react';
-import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../services/firebase";
 
@@ -16,9 +22,10 @@ type AuthContextType = {
     user: User | undefined;
     signInWithGoogle: () => Promise<void>;
     signUpWithEmailAndPassword: (email: string, password: string, displayName: string) => Promise<void>;
-    signInUserWithEmailAndPassword: (email: string, password: string) => Promise<void>; // Renomeado
+    signInUserWithEmailAndPassword: (email: string, password: string) => Promise<void>;
     deslogar: () => Promise<void>;
     updateName: (displayNameInput: string) => Promise<void>;
+    updateAvatar: (avatarUrl: string) => Promise<void>; // Nova função para atualizar o avatar
 }
 
 type AuthContextProviderProps = {
@@ -38,13 +45,14 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         const unsubscribe = auth.onAuthStateChanged(user => {
             if (user) {
                 const { displayName, photoURL, uid, email } = user;
-                if (!displayName || !photoURL || !email) {
+                if (!displayName || !email) {
                     throw new Error('Missing information from Google Account.');
                 }
+                const avatar = photoURL || "https://palestraparaprofessores.com.br/wp-content/webp-express/webp-images/uploads/2022/12/fotos-para-perfil-do-whatsapp.png.webp";
                 setUser({
                     id: uid,
                     name: displayName,
-                    avatar: photoURL,
+                    avatar: avatar,
                     email: email
                 });
             }
@@ -60,14 +68,15 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         const result = await signInWithPopup(auth, provider);
 
         if (result.user) {
-            const { displayName, photoURL, uid, email } = result.user;
-            if (!displayName || !photoURL || !email) {
+            const { displayName, uid, email } = result.user;
+            if (!displayName || !email) {
                 throw new Error('Missing information from Google Account.');
             }
+            const defaultAvatar = "https://palestraparaprofessores.com.br/wp-content/webp-express/webp-images/uploads/2022/12/fotos-para-perfil-do-whatsapp.png.webp";
             const newUser: User = {
                 id: uid,
                 name: displayName,
-                avatar: photoURL,
+                avatar: defaultAvatar,
                 email: email
             };
             setUser(newUser);
@@ -79,7 +88,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
             if (!snapshot.exists()) {
                 set(userRef, {
                     name: displayName,
-                    avatar: photoURL,
+                    avatar: defaultAvatar,
                     email: email,
                     progress: {
                         addition: {
@@ -115,12 +124,13 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
             const result = await createUserWithEmailAndPassword(auth, email, password);
 
             if (result.user) {
-                await updateProfile(result.user, { displayName });
-                const { uid, photoURL } = result.user;
+                const defaultAvatar = "https://palestraparaprofessores.com.br/wp-content/webp-express/webp-images/uploads/2022/12/fotos-para-perfil-do-whatsapp.png.webp";
+                await updateProfile(result.user, { displayName, photoURL: defaultAvatar });
+                const { uid } = result.user;
                 const newUser: User = {
                     id: uid,
                     name: displayName,
-                    avatar: photoURL || '',
+                    avatar: defaultAvatar,
                     email: email
                 };
                 setUser(newUser);
@@ -130,7 +140,7 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
                 const userRef = ref(db, 'users/' + uid);
                 await set(userRef, {
                     name: displayName,
-                    avatar: photoURL || '',
+                    avatar: defaultAvatar,
                     email: email,
                     progress: {
                         addition: {
@@ -164,7 +174,6 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         }
     };
 
-
     const signInUserWithEmailAndPassword = async (email: string, password: string) => {
         try {
             const userCredential = await firebaseSignInWithEmailAndPassword(auth, email, password);
@@ -172,10 +181,11 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
 
             if (user) {
                 const { uid, displayName, photoURL, email } = user;
+                const avatar = photoURL || "https://palestraparaprofessores.com.br/wp-content/webp-express/webp-images/uploads/2022/12/fotos-para-perfil-do-whatsapp.png.webp";
                 const newUser: User = {
                     id: uid,
                     name: displayName || '',
-                    avatar: photoURL || '',
+                    avatar: avatar,
                     email: email || ''
                 };
                 setUser(newUser);
@@ -188,8 +198,6 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
             console.error(`Error signing in: ${errorCode} - ${errorMessage}`);
         }
     };
-
-
 
     const deslogar = async () => {
         signOut(auth).then(() => {
@@ -218,9 +226,40 @@ export function AuthContextProvider(props: AuthContextProviderProps) {
         }
     };
 
+    const updateAvatar = async (avatarUrl: string) => {
+        const currentUser = auth.currentUser;
+        if (currentUser) {
+            await updateProfile(currentUser, { photoURL: avatarUrl });
+
+            setUser(prevUser => {
+                if (prevUser) {
+                    const updatedUser = { ...prevUser, avatar: avatarUrl };
+                    localStorage.setItem('user', JSON.stringify(updatedUser));
+
+                    // Atualiza o avatar no banco de dados também
+                    const db = getDatabase();
+                    const userRef = ref(db, 'users/' + currentUser.uid);
+                    set(userRef, {
+                        ...updatedUser
+                    });
+
+                    return updatedUser;
+                }
+                return prevUser;
+            });
+        }
+    };
 
     return (
-        <AuthContext.Provider value={{ user, signInWithGoogle, signUpWithEmailAndPassword, signInUserWithEmailAndPassword, deslogar, updateName }}>
+        <AuthContext.Provider value={{
+            user,
+            signInWithGoogle,
+            signUpWithEmailAndPassword,
+            signInUserWithEmailAndPassword,
+            deslogar,
+            updateName,
+            updateAvatar
+        }}>
             {props.children}
         </AuthContext.Provider>
     );
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,83 @@
-export function Profile(){
+import './style.scss';
+import { useState } from 'react';
+import IMGTESTE from '../../assets/img/vecteezy_profile-icon-design-vector_5544718.jpg';
+import img1 from '../../assets/img/img1.jpg';
+import img2 from '../../assets/img/img2.jpg';
+import img3 from '../../assets/img/img3.png';
+import img4 from '../../assets/img/img4.png';
+import { useAuth } from '../../hooks/useAuth';
+
+export function Profile() {
+    const { user, updateAvatar } = useAuth();
+    const [modalOpen, setModalOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+    const handleModal = () => {
+        setModalOpen(!modalOpen);
+    };
+
+    const handleImageSelect = (imgSrc: string) => {
+        setSelectedImage(imgSrc);
+    };
+
+    const handleSubmit = async () => {
+        if (selectedImage && updateAvatar) {
+            await updateAvatar(selectedImage);
+            handleModal();
+        }
+    };
+
     return (
-        <div>
-            profile page
+        <div className='container-profile'>
+            <div className='profile'>
+                <img className='img-modal' src={user?.avatar || IMGTESTE} alt="TESTE" />
+                <button className='btn-modal' onClick={handleModal}>Mudar avatar</button>
+
+                <div className='info-profile'>
+                    <p>Nome: {user?.name}</p>
+                    <p>Email: {user?.email}</p>
+                </div>
+            </div>
+
+            <div className='conquistas'>
+                CONQUISTAS
+            </div>
+
+            {modalOpen && (
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <h1>Escolha seu avatar</h1> <br />
+                        <div className='div-img-profile'>
+                            <img
+                                src={img1}
+                                alt="Avatar 1"
+                                className={selectedImage === img1 ? 'selected' : ''}
+                                onClick={() => handleImageSelect(img1)}
+                            />
+                            <img
+                                src={img2}
+                                alt="Avatar 2"
+                                className={selectedImage === img2 ? 'selected' : ''}
+                                onClick={() => handleImageSelect(img2)}
+                            /> <br />
+                            <img
+                                src={img3}
+                                alt="Avatar 3"
+                                className={selectedImage === img3 ? 'selected' : ''}
+                                onClick={() => handleImageSelect(img3)}
+                            />
+                            <img
+                                src={img4}
+                                alt="Avatar 4"
+                                className={selectedImage === img4 ? 'selected' : ''}
+                                onClick={() => handleImageSelect(img4)}
+                            />
+                        </div>
+                        <button onClick={handleSubmit}>ENVIAR</button>
+                        <button className='btn-close' onClick={handleModal}>X</button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
